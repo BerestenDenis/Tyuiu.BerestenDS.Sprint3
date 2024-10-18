9fd9910044ad7b8809a9e1069f4b0e644b4e0c71
Денис Берестень <d.blyl1337@gmail.com>
2024-10-18 19:58:50 +0500

    Task 0

--- Tyuiu.BerestenDS.Sprint3.Task0.V18.Lib/DataService.cs
+++ Tyuiu.BerestenDS.Sprint3.Task0.V18.Lib/DataService.cs
@@ -5,11 +5,11 @@ namespace Tyuiu.BerestenDS.Sprint3.Task0.V18.Lib
     {
         public double GetMultiplySeries(int value, int startValue, int stopValue)
         {
-            double sumSerios = 0;
+            double sumSerios = 1;
             int i;
             for (i = startValue; i <= stopValue; i++)
             {
-                sumSerios = sumSerios + Math.Pow(2/(Math.Cos(value)+0.5), i);
+                sumSerios = sumSerios * Math.Pow(2/(Math.Cos(value)+0.5), i);
             }
             return Math.Round(sumSerios, 3);
         }
