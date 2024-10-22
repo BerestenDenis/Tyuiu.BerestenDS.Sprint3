e302cb8056e74ef34eb47905b6ff792d45864cbc
Денис Берестень <d.blyl1337@gmail.com>
2024-10-22 23:59:44 +0500

    Task 4

---
+++ Tyuiu.BerestenDS.Sprint3.Task4.V1.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+using Tyuiu.BerestenDS.Sprint3.Task4.V1.Lib;
+namespace Tyuiu.BerestenDS.Sprint3.Task4.V1.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void Valid()
+        {
+            DataService ds = new DataService();
+            int startValue = -5;
+            int stopValue = 5;
+
+            double res = ds.Calculate(startValue, stopValue);
+            double wait =;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.BerestenDS.Sprint3.Task4.V1/Program.cs
@@ -0,0 +1,25 @@
+﻿using Tyuiu.BerestenDS.Sprint3.Task4.V1.Lib;
+class Program
+{
+    static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+        Console.Title = "Спринт #3 | Выполнил: Берестень Д. С.  | ИБКСБ-24-1";
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #3                                                               *");
+        Console.WriteLine("* Тема: Использование операторов continue и break в циклах                *");
+        Console.WriteLine("* Задание #4                                                              *");
+        Console.WriteLine("* Вариант #1                                                              *");
+        Console.WriteLine("* Выполнил: Берестень Денис Сергеевич  | ИБКСБ 24-1                       *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+        int startValue = -5;
+        int stopValue = 5;
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine(ds.Calculate(startValue, stopValue));
+    }
+}
