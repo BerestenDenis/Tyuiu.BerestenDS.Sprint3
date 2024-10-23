bd460e3915bcfb49652bba917240ea25a68af637
Денис Берестень <d.blyl1337@gmail.com>
2024-10-24 00:17:00 +0500

    Task 5

--- Tyuiu.BerestenDS.Sprint3.Task5.V13.Lib/DataService.cs
+++ Tyuiu.BerestenDS.Sprint3.Task5.V13.Lib/DataService.cs
@@ -6,12 +6,12 @@ namespace Tyuiu.BerestenDS.Sprint3.Task5.V13.Lib
         public double GetSumSumSeries(int x, int startValue1, int startValue2, int stopValue1, int stopValue2)
         {
             double sum = 0;
-            int i,j;
+            int i,k;
             for (i = startValue1; i <= stopValue1; i++)
             {
-                for (j = startValue2; j <= stopValue2; j++)
+                for (k = startValue2; k <= stopValue2; k++)
                 {
-                    sum = sum + (Math.Cos(x) + (j / 2));
+                    sum = sum + (Math.Cos(x) + (k / 2.0));
                 }
             }
             return Math.Round(sum,3);
